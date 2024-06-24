461c6fb2bee16087166ca0801dbebf8b1f046500
gz65555 <gz65555@gmail.com>
2024-06-24 19:29:09 +0800

    remove useless content

--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -130,8 +130,6 @@ const App: React.FC = () => {
 
   return (
     <TypingContext.Provider value={{ state: state, dispatch }}>
-      <StarCard />
-      {state.isFinished && <DonateCard />}
       {state.isFinished && <ResultScreen />}
       <Layout>
         <Header>
